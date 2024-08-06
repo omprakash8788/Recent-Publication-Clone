223f1a097daba8a042650f85314d0b992e3e33c6
omprakash8788 <omprakashkumar552@gmail.com>
2024-08-06 15:12:44 +0530

    Ignore lib directory

--- blog-app/lib/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB= async()=>{
-    await mongoose.connect('mongodb+srv://food:food@cluster0.1u3ejlu.mongodb.net/recent-publication')
- console.log('DB Connected');
- 
-}
